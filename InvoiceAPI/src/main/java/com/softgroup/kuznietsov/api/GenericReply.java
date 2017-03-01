60fb3bf4e97d5e1ca01b6a353559efc9bcbb7275
Vitalii <8imposible8@gmail.com>
2017-03-01 22:04:29 +0200

    First class

---
+++ InvoiceAPI/src/main/java/com/softgroup/kuznietsov/api/GenericReply.java
@@ -0,0 +1,14 @@
+package com.softgroup.kuznietsov.api;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class GenericReply {
+    @XmlElement(required=true)
+    public Integer retcode = 0;
+    @XmlElement(required=true)
+    public String apiVer = "0.0.1";
+    @XmlElement(required=false)
+    public String error_message;
+}
