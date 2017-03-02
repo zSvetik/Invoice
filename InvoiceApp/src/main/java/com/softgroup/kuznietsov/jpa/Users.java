30ab85c234c0ab47fc874564f313f4e519ab878e
Vitalii <8imposible8@gmail.com>
2017-03-02 14:30:30 +0200

    Update and rename User.java to Users.java

---
+++ InvoiceApp/src/main/java/com/softgroup/kuznietsov/jpa/Users.java
@@ -0,0 +1,131 @@
+package com.softgroup.kuznietsov.jpa;
+
+import java.io.Serializable;
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "users")
+public class Users implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Long id;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "login")
+    private String login;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "password")
+    private String password;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "balance")
+    private Long balance;
+
+    @ManyToMany(mappedBy = "usersList", cascade = CascadeType.DETACH)
+    private List<Role> roleList = new ArrayList<>();
+
+    @JoinColumn(name = "id_room", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Room room;
+
+    public Users() {
+    }
+
+    public Users(Long id, String login, String password, Long balance) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public List<Role> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<Role> roleList) {
+        this.roleList = roleList;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Users other = (Users) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Users [id=" + id + ", login=" + login + ", balance=" + balance + "]";
+    }
+}
