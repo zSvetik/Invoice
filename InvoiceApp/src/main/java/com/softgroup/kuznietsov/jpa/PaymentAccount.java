96b3b3ce6323d9c292290ce57dc475aadb4d74cf
Vitalii <8imposible8@gmail.com>
2017-03-02 14:31:35 +0200

    Update PaymentAccount.java

--- InvoiceApp/src/main/java/com/softgroup/kuznietsov/jpa/PaymentAccount.java
+++ InvoiceApp/src/main/java/com/softgroup/kuznietsov/jpa/PaymentAccount.java
@@ -1,13 +1,108 @@
 package com.softgroup.kuznietsov.jpa;
 
 import java.io.Serializable;
+import java.util.*;
+
 import javax.persistence.*;
-import java.math.BigInteger;
-import java.util.List;
+import javax.validation.constraints.*;
 
+@Entity
+@Table(name = "payment_account")
+public class PaymentAccount implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-/**
- * The persistent class for the payment_account database table.
- * 
- */
-public class PaymentAccount implements Serializable {}
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Long id;
+
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "payment_account")
+    private Long paymentAccount;
+
+    @JoinColumn(name = "mfo_bank", referencedColumnName = "mfo")
+    @ManyToOne(optional = false)
+    private Bank bank;
+
+    @JoinColumn(name = "edrpou_organization", referencedColumnName = "edrpou")
+    @ManyToOne(optional = false)
+    private Organization organization;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "paymentAccount")
+    private List<Invoice> invoiceList = new ArrayList<>();
+
+    public PaymentAccount() {
+    }
+
+    public PaymentAccount(Long id, Long paymentAccount, Bank bank, Organization organization) {
+        this.id = id;
+        this.paymentAccount = paymentAccount;
+        this.bank = bank;
+        this.organization = organization;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPaymentAccount() {
+        return paymentAccount;
+    }
+
+    public void setPaymentAccount(Long paymentAccount) {
+        this.paymentAccount = paymentAccount;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PaymentAccount other = (PaymentAccount) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentAccount [id=" + id + "]";
+    }
+
+}
