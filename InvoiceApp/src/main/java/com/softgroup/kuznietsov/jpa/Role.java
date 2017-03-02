058b8901f2cd8c81ce70661b55339f6820bb66b5
Vitalii <8imposible8@gmail.com>
2017-03-02 14:30:50 +0200

    Update Role.java

--- InvoiceApp/src/main/java/com/softgroup/kuznietsov/jpa/Role.java
+++ InvoiceApp/src/main/java/com/softgroup/kuznietsov/jpa/Role.java
@@ -1,12 +1,92 @@
 package com.softgroup.kuznietsov.jpa;
 
 import java.io.Serializable;
+import java.util.*;
+
 import javax.persistence.*;
-import java.util.List;
+import javax.validation.constraints.*;
 
+@Entity
+@Table(name = "role")
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-/**
- * The persistent class for the role database table.
- * 
- */
-public class Role implements Serializable {}
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Long id;
+
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "role")
+    private String role;
+
+    @JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "id", referencedColumnName = "id_role") }, inverseJoinColumns = {
+            @JoinColumn(name = "id", referencedColumnName = "id_user") })
+    @ManyToMany(cascade = CascadeType.DETACH)
+    private List<Users> usersList = new ArrayList<>();
+
+    public Role() {
+    }
+
+    public Role(Long id, String role) {
+        this.id = id;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<Users> getUsersList() {
+        return usersList;
+    }
+
+    public void setUsersList(List<Users> usersList) {
+        this.usersList = usersList;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Role other = (Role) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", role=" + role + "]";
+    }
+}
