1dc760ad8dfd668dc94f584c0d9501adca4dde6d
Vitalii <8imposible8@gmail.com>
2017-03-10 11:39:54 +0200

    Update EntityIdGenerator.java

--- InvoiceApp/src/main/java/com/softgroup/kuznietsov/utils/EntityIdGenerator.java
+++ InvoiceApp/src/main/java/com/softgroup/kuznietsov/utils/EntityIdGenerator.java
@@ -6,8 +6,11 @@ import java.util.UUID;
  * Created by User on 27.02.2017.
  */
 public class EntityIdGenerator {
-
     public static Long random(){
-        return UUID.randomUUID().getLeastSignificantBits();
+        Long l = UUID.randomUUID().getLeastSignificantBits();
+        if(l<0){
+            l=-l;
+        }
+        return l;
     }
 }
